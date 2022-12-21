cc883a880c09e9af4ace300b6fcf2f07e0279715
sebalp100 <sebalp100@gmail.com>
2022-12-21 16:29:16 -0300

    Added active and inactive nav bar effect

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,19 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
+import { Link, useLocation } from 'react-router-dom';
 import { FaGrin } from 'react-icons/fa';
 
 function Header() {
+  const location = useLocation();
+  const [url, setUrl] = useState(null);
+  useEffect(() => {
+    setUrl(location.pathname);
+  }, [location]);
   return (
     <nav>
       <h1>Bookstore CMS</h1>
       <div className="link">
-        <li><Link to="/" style={{ textDecoration: 'none' }}>BOOKS</Link></li>
-        <li><Link to="/Categories" style={{ textDecoration: 'none' }}>CATEGORIES</Link></li>
+        <li><Link to="/" className={`${url === '/' ? ' active' : 'inactive'}`}>BOOKS</Link></li>
+        <li><Link to="/Categories" className={`${url === '/Categories' ? ' active' : 'inactive'}`}>CATEGORIES</Link></li>
       </div>
       <div className="iconContainer">
         <div className="user">
